e2c3b4bbe0e5cf161ffb4b10c2a34a982d445169
Laura Pino <lpigueroa@gmail.com>
2024-07-03 15:47:48 +0200

    Footer finished

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="mainFooterContainer">
+        <span>© Copyright 2024. All Rights Reserved</span>
+        <span>Design by AirCare</span>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
