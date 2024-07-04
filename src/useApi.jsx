9b10b2e5592ae0b66a77178118d8a0fb684f18b2
ArturoAlvarezY <arturoavilamarketing@gmail.com>
2024-07-04 17:40:46 +0200

    refactoring API code

--- src/useApi.jsx
+++ src/useApi.jsx
@@ -26,34 +26,3 @@ export const UseApi = (url) =>{
 
 }
 export default UseApi; 
-
-
-//-------------metodo 2---------------
-
-/* const UseApi = (url) =>{
-    const [data, setData] = useState(null)
-    const [error, setError] = useState(null)
-
-    useEffect(()=>{ 
-        const getData = async() =>{
-            try{
-                const response = await fetch(url);
-                if(!response.ok){
-                    throw new Error (`Error! status: ${response.status}`);
-                }
-                const json_data = await response.json();
-                setData(json_data); 
-    
-            }catch(error){
-
-            setError(error.message);
-            setError(error.message);
-                console.error(`Error fetching data: ${error}`);
-            }
-        }
-        getData(url);
-    }, [url]);
-    return{data,error};
-}
-export default UseApi; 
- */
--- src/useApi.test.jsx
+++
@@ -1,7 +0,0 @@
-import {describe, it, expect} from "vite";
-import { UseApi } from "../src/useApi"
-describe('UseApi', () => {
-    it('should', () => {
-        
-    })
-})
